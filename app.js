14c1ad7bacd04155ee403a65eefc7cb243997c4a
Esteban Ariza Acosta <48232743+Esarac@users.noreply.github.com>
2022-05-04 21:39:46 -0500

    new API's

--- app.js
+++ app.js
@@ -1,38 +1,66 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const app = express();
+const PORT = 12345;
 
 app.use(bodyParser.json());
 
-const PORT = 12345;
-
-let obj = {
-    user : 'Mateo',
-};
-
-let obj2 = [
-    {
-        user: 'Mateo',
-        missingWork : 2
-    },
-    {
-        user: 'Mario',
-        missingWork : 3,
+class Student{
+    constructor(name, lastname, studentCode, identificationCard, missingWork){
+        this.name = name;
+        this.lastname = lastname;
+        this.studentCode = studentCode;
+        this.identificationCard = identificationCard;
+        this.missingWork = missingWork;
     }
+}
+
+let students = [
+    new Student('Mateo','Valdes Otero','1','1',1),
+    new Student('Esteban','Ariza Acosta','2','2',1),
+    new Student('Samuel','Satizabal Tascon','3','3',1),
+    new Student('Johan Sebastian','Giraldo Rubio','4','4',1),
+    new Student('Juan David','Ossa Ossa','5','5',1),
+    new Student('Ernesto','N/A','6','6',1),
+    new Student('Christian','Flor Astudillo','7','7',1),
 ];
 
+//Methods
+function loadCSV(csv){
+    console.log("LOADED :)");
+}
+
 //Routes
-app.get('/', (req, res) => {
-    res.send(obj2);
+app.get('/students', (req, res) => {
+    res.send(students);
 });
 
-app.post('/students', (req, res) => {
-    let a = req.body.students;
-    console.log(a);
-    res.send(obj2);
+app.post('/students/identificationcard', (req, res) => {
+    let value = req.body.identificationCard;
+
+    let info = undefined;
+    students.forEach(i =>{
+        if(i.identificationCard === value)
+            info = i;
+    });
+    
+    res.send(info);
 });
 
+app.post('/students/studentCode', (req, res) => {
+    let value = req.body.studentCode;
 
+    let info = undefined;
+    students.forEach(i =>{
+        if(i.studentCode === value)
+            info = i;
+    });
+    
+    res.send(info);
+});
+
+//Port
 app.listen(PORT, () => {
+    loadCSV("students.csv");
     console.log(`Started on port: ${PORT}`);
 });
