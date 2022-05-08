dc6357bf147eb4d37d2add8fc7cc2de35b72485d
Esteban Ariza Acosta <48232743+Esarac@users.noreply.github.com>
2022-05-08 17:35:46 -0500

    query :question:

--- app.js
+++ app.js
@@ -6,34 +6,79 @@ const PORT = 12345;
 app.use(bodyParser.json());
 
 class Student{
-    constructor(name, lastname, studentCode, identificationCard, missingWork){
+    constructor(name, lastname, studentCode, identificationCard, missingWork, weightedAverage){
         this.name = name;
         this.lastname = lastname;
         this.studentCode = studentCode;
         this.identificationCard = identificationCard;
         this.missingWork = missingWork;
+        this.weightedAverage = weightedAverage;
     }
 }
 
 let students = [
-    new Student('Mateo','Valdes Otero','1','1',1),
-    new Student('Esteban','Ariza Acosta','2','2',1),
-    new Student('Samuel','Satizabal Tascon','3','3',1),
-    new Student('Johan Sebastian','Giraldo Rubio','4','4',1),
-    new Student('Juan David','Ossa Ossa','5','5',1),
-    new Student('Ernesto','N/A','6','6',1),
-    new Student('Christian','Flor Astudillo','7','7',1),
+    new Student('Mateo','Valdes Otero','1','1',1, 5),
+    new Student('Esteban','Ariza Acosta','2','2',2, 4.5),
+    new Student('Samuel','Satizabal Tascon','3','3',3, 4.2),
+    new Student('Johan Sebastian','Giraldo Rubio','4','4',4, 4.3),
+    new Student('Juan David','Ossa Ossa','5','5',5, 3.5),
+    new Student('Ernesto','N/A','6','6',2, 3.0),
+    new Student('Christian','Flor Astudillo','7','7',3, 4.8),
 ];
 
 //Methods
+const logicalComparator = {
+    '<': function (x, y) { return x < y },
+    '<=': function (x, y) { return x <= y },
+    '=': function (x, y) { return x == y },
+    '>=': function (x, y) { return x >= y },
+    '>': function (x, y) { return x > y },
+    '<>': function (x, y) { return x != y },
+};
+
 function loadCSV(csv){
     console.log("LOADED :)");
 }
 
+function findStudentsStudentCode(studentCodes){
+    let info = [];
+    students.forEach(i =>{
+        studentCodes.forEach(j =>{
+            if(i.studentCode === j){
+                info.push(i);
+            }
+        });
+    });
+    
+    return info;
+}
+
 //Routes
 app.get('/students', (req, res) => {
+    console.log("Request at /students");
     res.send(students);
 });
+
+app.post('/students/query/:data', (req, res) => {
+    let studentCodes = req.body.students;
+    let column = req.params.data;
+    let operator = req.body.operator;
+    let value = parseFloat(req.body.value);
+
+    let studentsQuery = students;
+    if(typeof studentCodes !== 'undefined')
+        studentsQuery = findStudentsStudentCode(studentCodes);
+
+    let info = [];
+    studentsQuery.forEach(i =>{
+        if(logicalComparator[operator](i[column], value))
+            info.push(i['studentCode']);
+    });
+    
+    console.log("Request at /students/query");
+    res.send(info);
+});
+
 
 app.post('/students/identificationcard', (req, res) => {
     let value = req.body.identificationCard;
@@ -44,6 +89,7 @@ app.post('/students/identificationcard', (req, res) => {
             info = i;
     });
     
+    console.log("Request at /students/identificationcard");
     res.send(info);
 });
 
@@ -56,6 +102,7 @@ app.post('/students/studentCode', (req, res) => {
             info = i;
     });
     
+    console.log("Request at /students/studentCode");
     res.send(info);
 });
 
